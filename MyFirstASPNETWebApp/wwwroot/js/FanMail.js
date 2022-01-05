2853223d1e64230aa7f41a630ae50124ac8b0203
dongkya <kdh2395478@gmail.com>
2022-01-05 14:49:51 -0700

    초기 커밋.

---
+++ MyFirstASPNETWebApp/wwwroot/js/FanMail.js
@@ -0,0 +1,13 @@
+﻿//$("#btnsend").addeventlistener("click", function () {
+//    alert("from: " + $("#txtfrom").value +
+//        ", subject: " + $("#txtsubject").value +
+//        ", message: " + $("#txtmessage").value);
+//})
+
+$("#btnSend").click(function () {
+    alert("From: " + $("#txtFrom").val() +
+        ", Subject: " + $("#txtSubject").val() +
+        ", Message: " + $("#txtMessage").val());
+
+    $("#picGanderson").fadeToggle();
+})
